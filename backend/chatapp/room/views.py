6d0541b9a8934fdc43c7fcfd079dd953c4fd8bec
Wanderley <wanderley@ibm.com>
2021-05-08 23:23:53 -0300

    chat almost ready

--- backend/chatapp/room/views.py
+++ backend/chatapp/room/views.py
@@ -8,6 +8,8 @@ def index(request):
     return render(request, 'chat/index.html')
 
 def room(request, room_name):
+    #TODO: Check if room Exists
+    # MOVE API TO api direcotry
     return render(request, 'chat/room.html',{
         'room_name': room_name
     })
